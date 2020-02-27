69e1e6a6ea4e98ee3a3d4aa1e18d2ec325845d7b
Nick Pafundi <npafundi@gavant.com>
2020-02-27 16:42:44 -0500

    updated tests

--- tests/integration/components/date-input-mask-test.ts
+++ tests/integration/components/date-input-mask-test.ts
@@ -26,9 +26,9 @@ module('Integration | Component | date-input-mask', function(hooks) {
 
     test('the input mask works with a custom mask and custom mutedMask', async function(assert) {
         this.set('mask', '99-99-9999');
-        this.set('mutedMask', 'mm-dd-YYYY');
+        this.set('maskPlaceholder', 'mm-dd-YYYY');
 
-        await render(hbs`<DateInputMask @mask={{this.mask}} @mutedMask={{this.mutedMask}}/>`);
+        await render(hbs`<DateInputMask @mask={{this.mask}} @maskPlaceholder={{this.maskPlaceholder}}/>`);
         await fillIn('input', '12345678');
 
         const inputMask = this.element.querySelector('.input-mask input') as HTMLInputElement;
@@ -36,11 +36,11 @@ module('Integration | Component | date-input-mask', function(hooks) {
         assert.equal(inputMask?.value?.trim(), '12-34-5678');
     });
 
-    test('the muted mask is visible on focus and invisible on lost focus', async function(assert) {
+    test('the mask placeholder is visible on focus and invisible on lost focus', async function(assert) {
         await render(hbs`{{date-input-mask}}`);
         await focus('input');
 
-        var mutedMask = this.element.querySelector('.input-mask span') as HTMLInputElement;
+        let mutedMask = this.element.querySelector('.input-mask span') as HTMLInputElement;
         assert.equal(mutedMask?.textContent?.trim(), 'mm/dd/YYYY');
 
         await triggerEvent('input', 'focusout');
@@ -48,29 +48,29 @@ module('Integration | Component | date-input-mask', function(hooks) {
         assert.equal(mutedMask, null);
     });
 
-    test('the muted mask, invisible mask and mask values update on basic input', async function(assert) {
+    test('the mask placeholder, invisible placeholder and mask values update on basic input', async function(assert) {
         await render(hbs`{{date-input-mask}}`);
         await fillIn('input', '1');
         await focus('input');
 
-        var inputMask = this.element.querySelector('.input-mask input') as HTMLInputElement;
-        var mutedMask = this.element.querySelector('.input-mask span.mask') as HTMLSpanElement;
-        var invisibleMask = this.element.querySelector('.input-mask span span') as HTMLSpanElement;
+        let inputMask = this.element.querySelector('.input-mask input') as HTMLInputElement;
+        let maskPlaceholder = this.element.querySelector('.input-mask span.input-mask-value') as HTMLSpanElement;
+        let invisiblePlaceholder = this.element.querySelector('.input-mask span span') as HTMLSpanElement;
 
         assert.equal(inputMask?.value.trim(), '1');
-        assert.equal(mutedMask?.innerText?.trim(), 'm/dd/YYYY');
-        assert.equal(invisibleMask?.textContent?.trim(), '1');
+        assert.equal(maskPlaceholder?.innerText?.trim(), 'm/dd/YYYY');
+        assert.equal(invisiblePlaceholder?.textContent?.trim(), '1');
 
         await fillIn('input', '123');
 
         assert.equal(inputMask?.value.trim(), '12/3');
-        assert.equal(mutedMask?.innerText?.trim(), 'd/YYYY');
-        assert.equal(invisibleMask?.textContent?.trim(), '12/3');
+        assert.equal(maskPlaceholder?.innerText?.trim(), 'd/YYYY');
+        assert.equal(invisiblePlaceholder?.textContent?.trim(), '12/3');
 
         await fillIn('input', '12345');
 
         assert.equal(inputMask?.value.trim(), '12/34/5');
-        assert.equal(mutedMask?.innerText?.trim(), 'YYY');
-        assert.equal(invisibleMask?.textContent?.trim(), '12/34/5');
+        assert.equal(maskPlaceholder?.innerText?.trim(), 'YYY');
+        assert.equal(invisiblePlaceholder?.textContent?.trim(), '12/34/5');
     });
 });
